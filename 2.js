b662b7731d06523eb871cfc0e218eb4ffe3bf728
Aji Tio Kuswandhito <56579579+dhektio90@users.noreply.github.com>
2020-04-25 19:43:28 +0700

    Create 2.js
    menggunakan javascript

---
+++ 2.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" charset="utf-8">
+        // var data = [1,2,3,2,1];
+        // var jumlahBulan = data.length;
+        // document.write(jumlahBulan);
+        
+        document.write(<br />);
+            data = [1,2,3,2,1];
+        var jumlahHari = data.reduce((total, value)=>total+value,0);
+        document.write(jumlahHari);
+        
+        
+        
+        
+</script>
